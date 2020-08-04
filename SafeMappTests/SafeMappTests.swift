e7e995bfde61188c7878890bcb514374fc388b57
Aarón Granado <agranado@zimaltec.es>
2020-08-04 23:44:02 +0200

    Initial Commit

---
+++ SafeMappTests/SafeMappTests.swift
@@ -0,0 +1,34 @@
+//
+//  SafeMappTests.swift
+//  SafeMappTests
+//
+//  Created by Aarón on 4/8/20.
+//  Copyright © 2020 Aarón. All rights reserved.
+//
+
+import XCTest
+@testable import SafeMapp
+
+class SafeMappTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
